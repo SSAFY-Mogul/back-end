f6f4121ae8010ab4bfa0147ed69b13d98d9f7cec
On Yoo <dhsdb02@gmail.com>
2024-01-19 15:31:50 +0900

    initial project

---
+++ back-end/mogul/src/main/java/com/mogul/demo/MogulApplication.java
@@ -0,0 +1,13 @@
+package com.mogul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogulApplication.class, args);
+	}
+
+}
