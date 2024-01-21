d5d6c138d9e18b2f81c205d4ab8bc1b0f0c5866f
Sieun Kim <sieun043@gmail.com>
2024-01-21 17:15:51 +0900

    feat(webtoon):Update WebtoonRepository.find

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
@@ -1,10 +1,17 @@
 package com.mogul.demo.webtoon.repository;
 
 import com.mogul.demo.webtoon.entity.WebtoonEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public interface WebtoonRepository extends JpaRepository<WebtoonEntity, Long> {
 
+    @Query("select w from WebtoonEntity w order by w.grade")
+    List<WebtoonEntity> find(Pageable pageable) throws SQLException;
 }
