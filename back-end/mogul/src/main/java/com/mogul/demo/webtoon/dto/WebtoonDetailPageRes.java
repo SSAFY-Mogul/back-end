0590ab7bc161a3095e04e0d528845328305032ed
Sieun Kim <sieun043@gmail.com>
2024-01-21 20:39:54 +0900

    feat(webtoon):Update WebtoonDetailPageRes.java

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/dto/WebtoonDetailPageRes.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/dto/WebtoonDetailPageRes.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class WebtoonDetailPageRes extends ResUtil {
+    private Map<String, Object> data;
 }
