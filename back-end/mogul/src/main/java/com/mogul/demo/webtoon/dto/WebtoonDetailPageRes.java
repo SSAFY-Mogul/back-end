2a26ba37d979d45ca650f457bd31e8a641177cf5
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:59:23 +0900

    feat(webtoon):Add WebtoonDetailPageRes.java

---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/dto/WebtoonDetailPageRes.java
@@ -0,0 +1,12 @@
+package com.mogul.demo.webtoon.dto;
+
+import com.mogul.demo.common.util.ResUtil;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class WebtoonDetailPageRes extends ResUtil {
+}
