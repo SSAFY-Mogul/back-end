330690fcba79e8c447049d55ec0db253e5ecab3e
Sieun Kim <sieun043@gmail.com>
2024-01-21 17:16:17 +0900

    feat(webtoon):Update WebtoonServiceImpl.findWebtoonMain

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -4,6 +4,8 @@ import com.mogul.demo.webtoon.dto.WebtoonMainPageRes;
 import com.mogul.demo.webtoon.repository.WebtoonCntRepository;
 import com.mogul.demo.webtoon.repository.WebtoonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
@@ -24,8 +26,10 @@ public class WebtoonServiceImpl implements WebtoonService{
     public WebtoonMainPageRes findWebtoonMain(int page_number, int page_size) {
         WebtoonMainPageRes res = new WebtoonMainPageRes();
         Map<String, List> data = new HashMap<String, List>();
+        Pageable pageable = PageRequest.of(page_number, page_size);
         try{
-            
+            data.put("webtoon_hot_grade", webtoonRepository.find(pageable));
+
         }catch(SQLException e){
             e.printStackTrace();
         }
