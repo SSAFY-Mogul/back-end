de260b3a8aaa80b82c5213805ebe8e78c8eda8a5
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:50:59 +0900

    feat(webtoon):Update WebtoonRepository.findAllByGenre

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
@@ -4,6 +4,7 @@ import com.mogul.demo.webtoon.entity.WebtoonEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
@@ -13,8 +14,11 @@ import java.util.List;
 public interface WebtoonRepository extends JpaRepository<WebtoonEntity, Long> {
 
     @Query("select w from WebtoonEntity w order by w.grade")
-    List<WebtoonEntity> find(Pageable pageable) throws SQLException;
+    List<WebtoonEntity> findMain(Pageable pageable) throws SQLException;
 
     @Query("select w from WebtoonEntity w order by w.title")
     List<WebtoonEntity> findAllByTitle(Pageable pageable) throws SQLException;
+
+    @Query("select w from WebtoonEntity w where w.genre=:genre order by w.title")
+    List<WebtoonEntity> findAllByGenre(@Param("genre") String genre, Pageable pageable) throws SQLException;
 }
