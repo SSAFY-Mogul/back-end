79c64ef5c82b98208b6510af0f1dbe7bee621b39
Sieun Kim <sieun043@gmail.com>
2024-01-21 17:22:07 +0900

    feat(webtoon):Update WebtoonCntRepository.java

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
@@ -1,8 +1,16 @@
 package com.mogul.demo.webtoon.repository;
 
+import com.mogul.demo.webtoon.entity.WebtoonCntEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface WebtoonCntRepository {
+import java.util.List;
 
+@Repository
+public interface WebtoonCntRepository extends JpaRepository<WebtoonCntEntity, Long> {
+
+    @Query("select wc from WebtoonCntEntity wc order by wc.cnt")
+    List<WebtoonCntEntity> find(Pageable pageable);
 }
