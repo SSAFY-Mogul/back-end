8a524593afbacda8b777b92b7d937df258ffb00c
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:50:35 +0900

    feat(webtoon):Modify WebtoonCntRepository.find to WebtoonCntRepository.findMain

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 public interface WebtoonCntRepository extends JpaRepository<WebtoonCntEntity, Long> {
 
     @Query("select wc from WebtoonCntEntity wc order by wc.cnt")
-    List<WebtoonCntEntity> find(Pageable pageable);
+    List<WebtoonCntEntity> findMain(Pageable pageable);
 }
