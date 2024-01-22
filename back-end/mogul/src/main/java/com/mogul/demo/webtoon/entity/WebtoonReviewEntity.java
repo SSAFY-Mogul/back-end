361c8d4a82ea18103deb653488df67874dfc35a6
Sieun Kim <sieun043@gmail.com>
2024-01-22 16:42:54 +0900

    feat(webtoon):Delete WebtoonReviewEntity.java and WebtoonReviewRepository.java

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonReviewEntity.java
+++
@@ -1,4 +0,0 @@
-package com.mogul.demo.webtoon.entity;
-
-public class WebtoonReviewEntity {
-}
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonReviewRepository.java
+++
@@ -1,5 +0,0 @@
-package com.mogul.demo.webtoon.repository;
-
-public interface WebtoonReviewRepository {
-
-}
