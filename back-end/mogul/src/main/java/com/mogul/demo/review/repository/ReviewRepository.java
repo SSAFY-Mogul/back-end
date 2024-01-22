0f402b8dbd8c36a6dd28012068e965a0ed7cc8f1
Sieun Kim <sieun043@gmail.com>
2024-01-22 17:23:04 +0900

    feat(webtoon):Add ReviewRepository.java

---
+++ back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.mogul.demo.review.repository;
+
+import com.mogul.demo.review.entity.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+}
