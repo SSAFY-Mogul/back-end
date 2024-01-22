aea18d6828e15efee09cc03cd62f01b3205bc8e8
Sieun Kim <sieun043@gmail.com>
2024-01-22 17:29:06 +0900

    feat(webtoon): Update ReviewRepository.findByWebtoonId

--- back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
@@ -2,8 +2,15 @@ package com.mogul.demo.review.repository;
 
 import com.mogul.demo.review.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+
+    @Query("select * from ReviewEntity r where r.webtoonId=:webtoonId order by r.registeredDate desc")
+    List<ReviewEntity> findByWebtoonId(@Param("webtoonId") long webtoonId);
 }
