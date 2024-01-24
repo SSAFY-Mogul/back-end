47466ff7212cc9255597eacc9a415fb9751afbcd
Sieun Kim <sieun043@gmail.com>
2024-01-24 17:04:24 +0900

    feat(chat):Add Nickname Generator

---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/entity/NicknamePrefixEntity.java
@@ -0,0 +1,25 @@
+package com.mogul.demo.chat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "nickname_prefix")
+@Getter
+@Setter
+@NoArgsConstructor
+public class NicknamePrefixEntity {
+
+    @Id
+    @Column(name = "nickname_prefix_id", nullable = false)
+    private long id;
+
+    @Column(name = "nickname_prefix_value", nullable = false)
+    private String prefix;
+
+}
---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
@@ -0,0 +1,64 @@
+package com.mogul.demo.chat.nickname;
+
+import com.mogul.demo.chat.repository.NicknamePrefixRepository;
+import com.mogul.demo.chat.repository.NicknamePostfixRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+@Getter
+@Setter
+public class NicknameGenerator {
+
+    private Map<Integer, Set<String>> nicknames;
+
+    private long prefixCount;
+
+    private long postfixCount;
+
+    private Random random;
+
+    @Autowired
+    NicknamePrefixRepository nickNamePrefixRepository;
+
+    @Autowired
+    NicknamePostfixRepository nicknamePostfixRepository;
+
+    public NicknameGenerator(){
+        this.nicknames = new HashMap<>();
+        this.prefixCount = nickNamePrefixRepository.count();
+        this.postfixCount = nicknamePostfixRepository.count();
+        this.random = new Random();
+    }
+
+    String generateNickname(int chatRoomId){
+        if(!nicknames.containsKey(chatRoomId)){
+            nicknames.put(chatRoomId, new HashSet<>());
+        }
+        StringBuilder userId = new StringBuilder();
+        userId.append(nickNamePrefixRepository.findOneById(random.nextLong(prefixCount)+1).getPrefix());
+        userId.append(nicknamePostfixRepository.findOneById(random.nextLong(postfixCount)+1).getPostfix());
+        while(true){
+            userId.append(random.nextInt(10));
+            if(!nicknames.get(chatRoomId).contains(userId.toString()))
+                break;
+        }
+        nicknames.get(chatRoomId).add(userId.toString());
+        return userId.toString();
+    }
+
+    void RemoveNickname(int chatRoomId, String userId) {
+        if(nicknames.containsKey(chatRoomId)){
+            if(nicknames.get(chatRoomId).contains(userId)){
+                nicknames.remove(userId);
+            }
+            if(nicknames.get(chatRoomId).isEmpty()){
+                nicknames.remove(chatRoomId);
+            }
+        }
+    }
+}
