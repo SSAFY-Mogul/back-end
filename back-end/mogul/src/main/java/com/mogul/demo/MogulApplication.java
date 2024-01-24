d7f10278eb1851b5d23e0da9a710d66f3bcde28f
On Yoo <dhsdb02@gmail.com>
2024-01-24 14:00:37 +0900

    chore: Add security setting off

--- back-end/mogul/src/main/java/com/mogul/demo/MogulApplication.java
+++ back-end/mogul/src/main/java/com/mogul/demo/MogulApplication.java
@@ -1,9 +1,11 @@
 package com.mogul.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@SpringBootApplication(/*exclude = SecurityAutoConfiguration.class*/)
 public class MogulApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MogulApplication.class, args);
 	}
