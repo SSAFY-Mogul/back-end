3e105eab7f95600c9467bb757f733730103798a4
Sieun Kim <sieun043@gmail.com>
2024-01-24 19:56:58 +0900

    feat(chat):Add ChatMessageRepository to store chatting message

--- back-end/mogul/src/main/java/com/mogul/demo/chat/repository/ChatMessageRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/repository/ChatMessageRepository.java
@@ -3,6 +3,8 @@ package com.mogul.demo.chat.repository;
 import com.mogul.demo.chat.document.ChatMessageDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends MongoRepository<ChatMessageDocument, String> {
     List<ChatMessageDocument> findAllByChatRoomId(int chatRoomId);
 }
