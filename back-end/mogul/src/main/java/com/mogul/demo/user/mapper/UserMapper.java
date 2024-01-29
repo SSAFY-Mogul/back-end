67208e32e095124aba0d22dad7d4e77830c90b6f
신종욱 <rmm1@naver.com>
2024-01-29 14:50:55 +0900

    feat(core): Add user mapper

--- back-end/mogul/src/main/java/com/mogul/demo/user/mapper/UserMapper.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/mapper/UserMapper.java
@@ -2,15 +2,18 @@ package com.mogul.demo.user.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-import com.mogul.demo.board.dto.ArticleCreateRequest;
-import com.mogul.demo.board.entity.Article;
 import com.mogul.demo.user.dto.UserJoinRequest;
 import com.mogul.demo.user.entity.User;
 
 @Mapper
 public interface UserMapper {
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-	@Mapping(target = "userId", source = "userId")
+	@Mapping(target = "email", source = "email")
+	@Mapping(target = "password", source = "password")
+	@Mapping(target = "nickname", source = "nickname")
 	User userJoinRequestToUser(UserJoinRequest userJoinRequest);
 }
+
