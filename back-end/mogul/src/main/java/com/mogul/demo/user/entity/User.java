71e64e2b7bd79470720b82f017d13b7ebea4e972
신종욱 <rmm1@naver.com>
2024-01-29 17:39:15 +0900

    fix: Update User entity

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,4 +43,5 @@ public class User {
 
 	@Column(name = "user_is_deleted")
 	private Byte isDeleted = 0;
+
 }
