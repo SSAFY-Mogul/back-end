0a4f35618e7f4c257a99c1434b1501e4880b6943
Sieun Kim <sieun043@gmail.com>
2024-01-29 13:06:11 +0900

    feat(library):modify dto

--- back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewAddRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewAddRequest.java
@@ -13,25 +13,32 @@ import java.util.Date;
 public class ReviewAddRequest {
 
     private long webtoonId;
+
     private long userId;
+
     @NotBlank(message = "리뷰 제목은 비어 있을 수 없습니다.")
     @Size(max = 50, min = 1, message = "리뷰 제목은 50자 이하여야 합니다.")
     private String title;
+
     @NotBlank(message = "리뷰 내용은 비어 있을 수 없습니다.")
     @Size(max = 1000, min = 1, message = "리뷰 내용은 1000자 이하여야 합니다.")
     private String content;
+
     @NotNull(message = "작화 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "평점은 0점보다 작을 수 없습니다.")
     private int drawingScore;
+
     @NotNull(message = "스토리 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "평점은 0점보다 작을 수 없습니다.")
     private int storyScore;
+
     @NotNull(message = "연출 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "평점은 0점보다 작을 수 없습니다.")
     private int directingScore;
+
     private Date registeredDate;
 
 }
--- back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewUpdateRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewUpdateRequest.java
@@ -11,20 +11,25 @@ import lombok.NoArgsConstructor;
 public class ReviewUpdateRequest {
 
     private long id;
+
     @NotBlank(message = "리뷰 제목은 비어 있을 수 없습니다.")
     @Size(max = 50, min = 1, message = "리뷰 제목은 50자를 넘을 수 없습니다.")
     private String title;
+
     @NotBlank(message = "리뷰 내용은 비어있을 수 없습니다.")
     @Size(max = 1000, min = 1, message = "리뷰 내용은 1000자를 넘을 수 없습니다.")
     private String content;
+
     @NotNull(message = "작화 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "작화 평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "작화 평점은 1점보다 작을 수 없습니다.")
     private int drawingScore;
+
     @NotNull(message = "스토리 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "스토리 평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "스토리 평점은 1점보다 작을 수 없습니다.")
     private int storyScore;
+
     @NotNull(message = "연출 평점은 비어있을 수 없습니다.")
     @Max(value = 5, message = "연출 평점은 5점보다 클 수 없습니다.")
     @Min(value = 0, message = "연출 평점은 1점보다 작을 수 없습니다.")
