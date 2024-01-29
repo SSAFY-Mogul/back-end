5b7bcf80d22d15ef25f41c5cf92f4f08887f5403
Sieun Kim <sieun043@gmail.com>
2024-01-29 14:13:46 +0900

    feat(library):Docs review controller

--- back-end/mogul/src/main/java/com/mogul/demo/review/controller/ReviewController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/controller/ReviewController.java
@@ -5,6 +5,12 @@ import com.mogul.demo.review.dto.ReviewUpdateRequest;
 import com.mogul.demo.review.service.ReviewService;
 import com.mogul.demo.util.CustomResponse;
 import com.mogul.demo.webtoon.service.WebtoonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +23,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/review")
+@Tag(name = "Review", description = "리뷰 API")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -24,6 +31,14 @@ public class ReviewController {
     private final WebtoonService webtoonService;
 
     @PostMapping("/{webtoon-id}")
+    @Operation(summary = "리뷰 Create API", description = "리뷰를 등록하고 등록 성고 여부를 반환 합니다.", responses = {
+            @ApiResponse(responseCode = "200", description = "등록 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 웹툰, 등록 실패"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 형식")
+    }, parameters = {
+            @Parameter(name = "webtoonId", description = "리뷰를 등록하고자 하는 웹툰의 id"),
+            @Parameter(name = "reviewAddRequest", description = "등록하고자 하는 리뷰 내용", content = {@Content(schema = @Schema(implementation = ReviewAddRequest.class))})
+    })
     public ResponseEntity<CustomResponse> ReviewAdd(@PathVariable("webtoon-id") long webtoonId, @RequestBody ReviewAddRequest reviewAddRequest, BindingResult bindingResult){
         CustomResponse res;
         if(bindingResult.hasErrors()){
@@ -43,6 +58,13 @@ public class ReviewController {
     }
 
     @GetMapping("/{webtoon-id}")
+    @Operation(summary = "리뷰 Read API", description = "웹툰 id로 특정 웹툰의 관련 리뷰를 조회합니다.", responses = {
+            @ApiResponse(responseCode = "200", description = "조회 성공")
+    }, parameters = {
+            @Parameter(name = "webtoonId", description = "조회할 리뷰들의 웹툰 id"),
+            @Parameter(name = "pno", description = "조회할 리뷰의 페이지 번호"),
+            @Parameter(name = "count", description = "조회할 리뷰의 페이지 크기")
+    })
     public ResponseEntity<CustomResponse> reviewList(@PathVariable("webtoon-id") long webtoonId, @RequestParam("pno") int pageNumber, @RequestParam("count") int pageSize){
         List data = reviewService.findReviewsByWebtoonId(webtoonId, pageNumber, pageSize);
         CustomResponse res = new CustomResponse<List>(200, data, "리뷰 조회 성공");
@@ -50,6 +72,14 @@ public class ReviewController {
     }
 
     @PatchMapping("/{review-id}")
+    @Operation(summary = "리뷰 Update API", description = "특정 리뷰의 내용을 수정 합니다.", responses = {
+            @ApiResponse(responseCode = "200", description = "수정 성공"),
+            @ApiResponse(responseCode = "404", description = "수정 실패"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 형식")
+    }, parameters = {
+            @Parameter(name = "id", description = "수정할 리뷰의 id"),
+            @Parameter(name = "reviewUpdateRequest", description = "수정할 리뷰의 수정된 내용", content = {@Content(schema = @Schema(implementation = ReviewUpdateRequest.class))})
+    })
     public ResponseEntity<CustomResponse> reviewModify(@PathVariable("review-id") long id, @RequestBody @Valid ReviewUpdateRequest reviewUpdateRequest, BindingResult bindingResult){
         CustomResponse res;
         if(bindingResult.hasErrors()){
@@ -63,6 +93,12 @@ public class ReviewController {
     }
 
     @DeleteMapping("/{review-id}")
+    @Operation(summary = "리뷰 Delete API", description = "특정 리뷰를 삭제 합니다.", responses = {
+            @ApiResponse(responseCode = "200", description = "삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "리뷰를 찾을 수 없습니다, 삭제 실패")
+    }, parameters = {
+            @Parameter(name = "id", description = "삭제할 리뷰의 id")
+    })
     public ResponseEntity<CustomResponse> reviewRemove(@PathVariable("review-id") long id){
         boolean data = reviewService.removeReview(id);
         CustomResponse res = new CustomResponse<Boolean>(data?200:404, data, data?"리뷰 삭제 성공":"리뷰 삭제 실패");
