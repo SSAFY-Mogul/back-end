45c5094d8ee3940927d1ff6d73c645b990941107
신종욱 <rmm1@naver.com>
2024-01-29 23:24:07 +0900

    fix(user): Modify user DTO

--- back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserJoinRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserJoinRequest.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -17,10 +18,10 @@ public class UserJoinRequest {
 	private final String email;
 
 	@NotBlank
-	@Length(min = 8, max = 45)
+	@Pattern(regexp = "[A-Za-z0-9_!@#$]{8,45}$")
 	private final String password;
 
 	@NotBlank
-	@Length(min = 2, max = 15)
+	@Pattern(regexp = "[가-힣A-Za-z0-9]{2,15}")
 	private final String nickname;
 }
