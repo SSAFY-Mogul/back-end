0abc5a1000ce9d4fa89ba5b7ee50b7513239d922
신종욱 <rmm1@naver.com>
2024-01-31 01:09:42 +0900

    feat(user): Add JPA auditing

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -3,22 +3,31 @@ package com.mogul.demo.user.entity;
 import java.util.Date;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+
+/*
+ * Entity는 public 또는 protected 수준의 no-arg 생성자가 필요한데,
+ * protected로 설정할 경우 UserMapper 내부(UserMapperImpl)에서
+ * 생성자를 호출할 수 없어져 mapstruct에서 문제가 발생한다.
+ */
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
-@NoArgsConstructor
+@Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
