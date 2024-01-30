a3b8987c0cc5d3a9015f774fd3e4e362bdd17598
신종욱 <rmm1@naver.com>
2024-01-31 02:25:20 +0900

    feat(user): Add checking if user by email and nickname

--- back-end/mogul/src/main/java/com/mogul/demo/user/repository/UserRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/repository/UserRepository.java
@@ -11,9 +11,17 @@ import jakarta.annotation.Nonnull;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	//save()는 user 존재 여부에 따라 CREATE 또는 UPDATE
+
+	boolean existsByEmail(String email);
+
+
+	boolean existsByNickname(String nickname);
+
 
 	@Nonnull
 	Optional<User> findById(@Nonnull Long aLong);
+
 
 	@Nonnull
 	Optional<User> findByEmail(String email);
