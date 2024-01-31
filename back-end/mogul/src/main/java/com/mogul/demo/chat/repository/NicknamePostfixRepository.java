ab4253d38d5a7a78a5d4743666eb4dac43a9af71
Sieun Kim <sieun043@gmail.com>
2024-01-31 16:38:11 +0900

    feat(chat):modify unwrap input

--- back-end/mogul/src/main/java/com/mogul/demo/chat/repository/NicknamePostfixRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/repository/NicknamePostfixRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NicknamePostfixRepository extends JpaRepository<NicknamePostfixEntity, Long> {
-    NicknamePostfixEntity findOneById(Long id);
+    NicknamePostfixEntity findOneById(long id);
 }
--- back-end/mogul/src/main/java/com/mogul/demo/chat/repository/NicknamePrefixRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/repository/NicknamePrefixRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NicknamePrefixRepository extends JpaRepository<NicknamePrefixEntity, Long> {
 
-    NicknamePrefixEntity findOneById(Long id);
+    NicknamePrefixEntity findOneById(long id);
 }
