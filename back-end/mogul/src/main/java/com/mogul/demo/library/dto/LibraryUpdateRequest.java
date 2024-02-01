227adbc50ada2f490732b1507af86045a0d5102a
Sieun Kim <sieun043@gmail.com>
2024-02-01 09:29:30 +0900

    Revert "feat(admin):Fix typo"
    This reverts commit 1a8ccf99e90b3970a52bec15043b1f7c95efa578.

--- back-end/mogul/src/main/java/com/mogul/demo/library/dto/LibraryUpdateRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/dto/LibraryUpdateRequest.java
@@ -15,7 +15,7 @@ public class LibraryUpdateRequest {
 
     @NotBlank(message = "서재 이름은 비어 있을 수 없습니다.")
     @Size(max = 20, message = "서재 이름은 20자를 넘을 수 없습니다.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9?!@#$%^&()/]*$", message = "특수문자는 허용하지 않습니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "특수문자는 허용하지 않습니다.")
     private String name;
 
     private Long id;
