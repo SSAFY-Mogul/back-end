735ef9bec7c27688e60bf5c215ec615f82b33adb
Sieun Kim <sieun043@gmail.com>
2024-02-01 09:43:09 +0900

    feat(library):Fix service and add transactional

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -5,10 +5,10 @@ import com.mogul.demo.library.entity.LibraryEntity;
 import com.mogul.demo.library.mapper.LibraryMapper;
 import com.mogul.demo.library.repository.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -31,35 +31,41 @@ public class LibraryServiceImpl implements LibraryService{
     private final LibraryUserRepository libraryUserRepository;
 
     @Override
-    public List<LibraryResponse> findLibrariesByWebtoonId(long webtoonId, int pageNumber, int pageSize) {
+    @Transactional(readOnly = true)
+    public List<LibraryResponse> findLibrariesByWebtoonId(Long webtoonId, int pageNumber, int pageSize) {
         return libraryWebtoonThumbnailRepository.findByWebtoonIdAndIsDeletedFalseOrderBySubscriberNumberDesc(webtoonId, PageRequest.of(pageNumber, pageSize))
                 .stream().map(LibraryMapper.INSTANCE::fromLibraryWebtoonThumbnailEntityToLibraryResponse).collect(Collectors.toList());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<LibraryResponse> findLibrariesHot(int pageNumber, int pageSize) {
         return libraryThumbnailRepository.findByIsDeletedFalse(PageRequest.of(pageNumber, pageSize, Sort.by("subscriberNumber").descending()))
                 .stream().map(LibraryMapper.INSTANCE::fromLibraryThumbnailEntityToLibraryResponse).collect(Collectors.toList());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<LibraryResponse> findLibrariesByUserId(long userId) {
         return libraryThumbnailRepository.findByUserIdAndIsDeletedFalseOrderByRegisteredDateDesc(userId)
                 .stream().map(LibraryMapper.INSTANCE::fromLibraryThumbnailEntityToLibraryResponse).collect(Collectors.toList());
     }
 
     @Override
-    public LibraryResponse findLibraryById(long libraryId) {
+    @Transactional(readOnly = true)
+    public LibraryResponse findLibraryById(Long libraryId) {
         return LibraryMapper.INSTANCE.fromLibraryThumbnailEntityToLibraryResponse(libraryThumbnailRepository.findOneById(libraryId));
     }
 
     @Override
+    @Transactional
     public Long addLibrary(LibraryCreateRequest libraryCreateRequest) {
         LibraryEntity libraryEntity = LibraryMapper.INSTANCE.fromLibraryCreateRequestToLibraryEntity(libraryCreateRequest);
         return libraryRepository.save(libraryEntity).getId();
     }
 
     @Override
+    @Transactional
     public boolean removeLibrary(long id) {
         if(!libraryRepository.existsByIdAndIsDeletedFalse(id)){
             return false;
@@ -69,6 +75,7 @@ public class LibraryServiceImpl implements LibraryService{
     }
 
     @Override
+    @Transactional
     public boolean addWebtoon(LibraryAddWebtoonRequest libraryAddWebtoonRequest) {
         if(!libraryRepository.existsByIdAndIsDeletedFalse(libraryAddWebtoonRequest.getId())){
             return false;
@@ -79,12 +86,14 @@ public class LibraryServiceImpl implements LibraryService{
     }
 
     @Override
-    public List<SubscriptionResponse> findSubscription(long userId, int pageNumber, int pageSize) {
+    @Transactional
+    public List<SubscriptionResponse> findSubscription(Long userId, int pageNumber, int pageSize) {
         return librarySubscriptionThumbnailRepository.findByUserId(userId, PageRequest.of(pageNumber, pageSize))
                 .stream().map(LibraryMapper.INSTANCE::fromLibrarySubscriptionThumbnailEntityToSubscriptionResponse).collect(Collectors.toList());
     }
 
     @Override
+    @Transactional
     public boolean addSubscription(SubcriptionRequest subcriptionRequest) {
         if(!libraryRepository.existsByIdAndIsDeletedFalse(subcriptionRequest.getLibraryId())){
             return false;
@@ -99,6 +108,7 @@ public class LibraryServiceImpl implements LibraryService{
     }
 
     @Override
+    @Transactional
     public boolean removeSubscription(SubscriptionCancelRequest subscriptionCancelRequest) {
         if(!libraryUserRepository.existsByLibraryIdAndUserId(subscriptionCancelRequest.getLibraryId(), subscriptionCancelRequest.getUserId())){
             return false;
@@ -108,6 +118,7 @@ public class LibraryServiceImpl implements LibraryService{
     }
 
     @Override
+    @Transactional
     public boolean modifyLibrary(LibraryUpdateRequest libraryUpdateRequest) {
         if(!libraryRepository.existsByIdAndIsDeletedFalse(libraryUpdateRequest.getId())){
             return false;
