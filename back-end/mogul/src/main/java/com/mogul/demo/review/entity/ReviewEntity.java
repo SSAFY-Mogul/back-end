f9feb98ef746183f8cfcdd48431824ac9bc2ba0d
Sieun Kim <sieun043@gmail.com>
2024-02-02 13:15:19 +0900

    feat(webtoon):Modify entity add @AllArgsContructor

--- back-end/mogul/src/main/java/com/mogul/demo/review/entity/ReviewEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/entity/ReviewEntity.java
@@ -1,10 +1,7 @@
 package com.mogul.demo.review.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
@@ -15,6 +12,7 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ReviewEntity {
 
