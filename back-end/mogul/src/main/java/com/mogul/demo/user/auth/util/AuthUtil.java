4ef41b07402afab54eb2404ea7fe671fffb4efb4
신종욱 <rmm1@naver.com>
2024-02-05 15:56:13 +0900

    feat(auth): Add authentication utils

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/util/AuthUtil.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/util/AuthUtil.java
@@ -1,12 +1,19 @@
 package com.mogul.demo.user.auth.util;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.mogul.demo.user.auth.exception.UnauthorizedException;
 import com.mogul.demo.user.dto.UserPrincipal;
 
 public class AuthUtil {
-	public static UserPrincipal getAuthenticationInfo() {
+	public static UserPrincipal getAuthenticationInfo() throws UnauthorizedException{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if(!auth.isAuthenticated()) {
+			throw new UnauthorizedException("인증 정보가 없습니다.");
+		}
+
 		return (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
 
