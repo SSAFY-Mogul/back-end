13be68a4d501ff6a4103f6bd2735e97c0fda57a8
신종욱 <rmm1@naver.com>
2024-02-07 17:40:14 +0900

    refactor(profile): Update required value

--- back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserInfoSetRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserInfoSetRequest.java
@@ -5,17 +5,21 @@ import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoSetRequest {
 	@NotBlank
 	@Pattern(regexp = "[가-힣A-Za-z0-9]{2,15}")
 	private String nickname;
 
-	@NotBlank
-	@Pattern(regexp = "[A-Za-z0-9_!@#$]{8,15}$")
-	private String password;
+	// @NotBlank
+	// @Pattern(regexp = "[A-Za-z0-9_!@#$]{8,15}$")
+	// private String password;
 }
