4e0abc69d76c59f2b6cb9213ab33ee5318f3e4df
신종욱 <rmm1@naver.com>
2024-02-07 14:44:33 +0900

    fix(profile): Add exception handling for method not allowed

--- back-end/mogul/src/main/java/com/mogul/demo/advice/GlobalExceptionHandler.java
+++ back-end/mogul/src/main/java/com/mogul/demo/advice/GlobalExceptionHandler.java
@@ -2,12 +2,11 @@ package com.mogul.demo.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.mogul.demo.util.CustomResponse;
 import com.mogul.demo.util.ErrorResponse;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -26,6 +25,13 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 		return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "올바르지 않은 입력값입니다."),
 			HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+		HttpRequestMethodNotSupportedException ex) {
+		return new ResponseEntity<>(new ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED.value(), "지원하지 않는 요청입니다."),
+			HttpStatus.METHOD_NOT_ALLOWED);
 	}
 }
 
