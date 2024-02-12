9250b3d49856bc8eb6e47d7fd6da0d2849558b38
On Yoo <dhsdb02@gmail.com>
2024-02-13 08:39:13 +0900

    feat(search): Add search by webtoon title

--- back-end/mogul/src/main/java/com/mogul/demo/search/controller/SearchController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/search/controller/SearchController.java
@@ -3,7 +3,6 @@ package com.mogul.demo.search.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +10,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mogul.demo.search.dto.WebtoonSearchResponse;
-import com.mogul.demo.search.service.SearchService;
+import com.mogul.demo.search.dto.WebtoonTotalSearchResponse;
+import com.mogul.demo.search.service.WebtoonSearchService;
 import com.mogul.demo.util.CustomResponse;
 
 @RestController
 @RequestMapping("api/search")
 public class SearchController {
-	private final SearchService searchService;
+	private final WebtoonSearchService webtoonSearchService;
 
-	public SearchController(SearchService searchService) {
-		this.searchService = searchService;
+	public SearchController(WebtoonSearchService webtoonSearchService) {
+		this.webtoonSearchService = webtoonSearchService;
 	}
 
-	@GetMapping("")
+	@GetMapping("/webtoon")
 	public ResponseEntity<CustomResponse> search(@RequestParam("keyword") String keyword){
-		List<WebtoonSearchResponse> list = searchService.search(keyword);
+		WebtoonTotalSearchResponse list = webtoonSearchService.totalSearch(keyword);
+		return ResponseEntity.ok(new CustomResponse(HttpStatus.OK.value(),list,"성공"));
+	}
+
+	@GetMapping("/webtoon/title")
+	public ResponseEntity<CustomResponse> searchByWebtoonTitle(@RequestParam("keyword") String keyword){
+		List<WebtoonSearchResponse> list = webtoonSearchService.findByTitle(keyword);
+		if(list.isEmpty()){
+			return ResponseEntity.ok(new CustomResponse(HttpStatus.NO_CONTENT.value(), "","데이터가 없습니다"));
+		}
 		return ResponseEntity.ok(new CustomResponse(HttpStatus.OK.value(),list,"성공"));
 	}
 }
