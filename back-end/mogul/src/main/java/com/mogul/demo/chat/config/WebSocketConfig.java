234e6df677031b0bd07b0326ea4ac314aacc53db
Sieun Kim <sieun043@gmail.com>
2024-02-14 16:02:11 +0900

    fix(chat): Update websocket config to allow cors

--- back-end/mogul/src/main/java/com/mogul/demo/chat/config/WebSocketConfig.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/config/WebSocketConfig.java
@@ -19,6 +19,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler, "/api/chat/**")
-                .addInterceptors( new ChatHandShakeInterceptor());
+                .addInterceptors( new ChatHandShakeInterceptor())
+                .setAllowedOrigins("*");
     }
 }
