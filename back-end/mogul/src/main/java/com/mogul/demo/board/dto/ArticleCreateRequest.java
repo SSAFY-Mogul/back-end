6019d9f52d727b744623daddd99ee739d69eab64
On Yoo <dhsdb02@gmail.com>
2024-02-19 17:41:30 +0900

    fix: Delete unuse package

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleCreateRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleCreateRequest.java
@@ -3,10 +3,6 @@ package com.mogul.demo.board.dto;
 
 import java.util.List;
 
-import com.mogul.demo.user.dto.UserRequest;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
