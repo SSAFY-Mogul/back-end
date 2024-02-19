9f9bc58290467380d504365a74acd7a83f81fa34
On Yoo <dhsdb02@gmail.com>
2024-02-19 15:34:50 +0900

    feat(user): Delete user service

--- back-end/mogul/src/main/java/com/mogul/demo/user/exception/DuplicateUserException.java
+++
@@ -1,7 +0,0 @@
-package com.mogul.demo.user.exception;
-
-public class DuplicateUserException extends RuntimeException {
-	public DuplicateUserException(String message) {
-		super(message);
-	}
-}
--- back-end/mogul/src/main/java/com/mogul/demo/user/role/Role.java
+++
@@ -1,6 +0,0 @@
-package com.mogul.demo.user.role;
-
-public enum Role {
-	ADMIN,
-	USER
-}
